7c1c82fa02d6d0e1124e7326ba3112ed903ec53a
Diogo <Diogo@Diogo>
2020-06-01 10:52:45 +0100

    Versão 2

--- TrabalhoFinal/src/Main.java
+++ TrabalhoFinal/src/Main.java
@@ -2,6 +2,36 @@
 public class Main {
 
 	public static void main(String[] args) {
+		PersonNode mariaTree = populateTheTree();
+		mariaTree.printTree(0);
+		
+		
+	}
 
+	
+	private static PersonNode populateTheTree() {
+		PersonNode treeRoot = new PersonNode("Maria", 1850);
+		
+		addChild(treeRoot, "Maria", "João", 1870);
+		addChild(treeRoot, "Maria", "Ana", 1873);
+		addChild(treeRoot, "Maria", "Joana", 1875);
+		addChild(treeRoot, "Maria", "Aristites", 1880);
+		addChild(treeRoot, "João", "William", 1888);
+		addChild(treeRoot, "João", "Liliana", 1900);
+		addChild(treeRoot, "Ana", "Roberto", 1898);
+		addChild(treeRoot, "Ana", "Antonio", 1898);
+		addChild(treeRoot, "Joana", "Pedro", 1900);
+		addChild(treeRoot, "Aristites", "Ricardo", 1901);
+		addChild(treeRoot, "William", "Lara", 1914);
+		addChild(treeRoot, "Liliana", "Jose", 1923);
+		addChild(treeRoot, "Liliana", "Toy", 1926);
+		addChild(treeRoot, "Antonio", "Claudio", 1918);
+		
+		return treeRoot;
+	}
+
+
+	private static void addChild(PersonNode treeRoot, String parent, String child, int year) {
+		treeRoot.addChildToPerson(parent, new PersonNode(child, year));
 	}
 }
--- TrabalhoFinal/src/PersonNode.java
+++ TrabalhoFinal/src/PersonNode.java
@@ -28,12 +28,47 @@ public class PersonNode {
 	
 	//metodos
 	
+	/**
+	 * adiciona filho
+	 * @param newChild
+	 */
+	public void addChild(PersonNode newChild) {
+		childrens.add(newChild);
+	}
+	
+	/**
+	 * adicionar filho a pessoa que queremos
+	 * @param name pessoa que queremos dar um filho
+	 * @param newChild  
+	 */
+	public void addChildToPerson(String name, PersonNode newChild) {
+		if(name.equals(getName())) {
+			addChild(newChild);
+		}
+		for (int i = 0; i < childrens.size(); i++) {
+			PersonNode child = childrens.get(i);
+			child.addChildToPerson(name, newChild);
+		}
+	}
 	
 	@Override
 	public String toString() {
 		return getName() + "(" + getYear() + ")";
 	}
 	
+	public void printTree(int degree) {
+		printNTab(degree);
+		System.out.println(toString());
+		for (int i = 0; i < childrens.size(); i++) {
+			PersonNode child = childrens.get(i);
+			child.printTree(degree + 1);
+		}
+	}
 	
+	private void printNTab(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.print("\t");
+		}
+	}
 	
 }
